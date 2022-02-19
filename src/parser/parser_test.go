dd4a1b3b1d75274bda289375e703d5f31375f793
Roy Touw <roy.touw@newstory.nl>
2022-02-19 11:39:13 +0100

    Added parser test helpers

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -203,6 +203,64 @@ func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
 	return true
 }
 
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
+	opExp, ok := exp.(*ast.InfixExpression)
+
+	if !ok {
+		t.Errorf("exp is not ast.Infixexpression, got=%T(%s)", exp, exp)
+		return false
+	}
+
+	if !testLiteralExpression(t, opExp.Left, left) {
+		return false
+	}
+
+	if !testLiteralExpression(t, opExp.Right, right) {
+		return false
+	}
+
+	if opExp.Operator != operator {
+		t.Errorf("exp.Operator is not '%s'; got=%q", operator, opExp.Operator)
+		return false
+	}
+
+	return true
+}
+
+func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{}) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(t, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(t, exp, v)
+	case string:
+		return testIdentifier(t, exp, v)
+	default:
+		t.Errorf("type of expression not handled, got=%T", exp)
+		return false
+	}
+}
+
+func testIdentifier(t *testing.T, il ast.Expression, value string) bool {
+	ident, ok := il.(*ast.Identifier)
+	if !ok {
+		t.Errorf("il not *ast.IntegerLiteral; got=%T", il)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s; got=%s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("integ.TokenLiteral not %s; got=%s", value, ident.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
 func (test *Suite) TestParsingInfixExpressions() {
 	infixTests := []struct {
 		input      string
