5886ca9f9edfe64298a83f127c97d5a931e5d7c7
Roy Touw <roy.touw@newstory.nl>
2022-10-23 21:32:32 +0200

    Added a map function

---
+++ src/helpers/map.go
@@ -0,0 +1,12 @@
+package helpers
+
+// Map maps function over slice without mutating input
+func Map[T, U any](slice []T, fn func(i T) U) []U {
+	output := make([]U, len(slice))
+
+	for i := 0; i < len(slice); i++ {
+		output[i] = fn(slice[i])
+	}
+
+	return output
+}
